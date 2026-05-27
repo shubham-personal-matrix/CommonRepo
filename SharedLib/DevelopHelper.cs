d25922eeb02e96c9be743cfdf729b050a0da87e9
shubham.bansal <shubham.bansal@matrixcomsec.com>
2026-05-27 16:34:49 +0530

    Initial shared library

---
+++ SharedLib/DevelopHelper.cs
@@ -0,0 +1,15 @@
+namespace SharedLib
+{
+    public class DeviceHelper
+    {
+        public string GetDeviceStatus()
+        {
+            return "Device Connected Successfully";
+        }
+
+        public string GetDeviceIP()
+        {
+            return "192.168.1.10";
+        }
+    }
+}
